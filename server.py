477073675d25398df7d446dd8ea0174d56451460
aquaseals <itzme.haya@gmail.com>
2025-08-09 14:57:41 -0400

    scrapping flask idea for now

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import socket
-import keyboard
 import sys
 
 s = socket.socket()
@@ -16,22 +15,23 @@ print('socket listening')
 c, addr = s.accept()
 print('got connect from ' + str(addr))
 
-while True:
 
-    message = input('type your message')
+while True:
+    message = input('💬 -> ')
 
     c.send(message.encode())
 
     clientMSG = c.recv(1024).decode()
 
     if clientMSG == 'quit' or clientMSG == 'exit':
-        print('chat ended')
+        print('they ended the chat')
         c.close()
         sys.exit()
 
     print(clientMSG)
 
     if message == 'quit' or message == 'exit': # check if user wants to end the chat
+        print('you ended the chat')
         c.close()
         sys.exit()
 
