a567abd092d00ae778eac2381f68e8251419ffbf
idksarah <sarah.p.ngai@gmail.com>
2025-08-09 15:08:36 -0700

    I HATE JONATHAN DONG

--- interpreter.py
+++ interpreter.py
@@ -3,66 +3,64 @@ import time
 from translate import MorseCodeTranslator
 from imageSearch import ImageSearch
 from openImage import OpenImage
-    
-translator = MorseCodeTranslator()
-search = ImageSearch()
-openImage = OpenImage()
 
-# OpenImage.openImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRngGF6K9KVSyUaa3wRET32_OHo47WJgmcug17bw5Z8CeW-7p7MhBHcCigfRAw&s')
+class Interpreter:
+    @staticmethod
+    def interpret():
+        translator = MorseCodeTranslator()
+        search = ImageSearch()
+        open_image = OpenImage()
 
-input = ''
-
-prev = False
-current = False
-gap = 0
-
-DEBOUNCE_TIME = .1
-SPACE_TIME = .5
-
-def addSpace():
-    global input
-    global gap
-    if gap > SPACE_TIME and len(input) > 0 and not (input[-1] == ' '):
-        input += ' '
+        morse_input = ''
+        prev = False
+        current = False
         gap = 0
 
-def imageSearch(p_text):
-    return search.getImageUrl(p_text)
+        DEBOUNCE_TIME = 0.1
+        SPACE_TIME = 0.5
 
-while True:
-    addSpace()
+        def add_space():
+            nonlocal morse_input, gap
+            if gap > SPACE_TIME and len(morse_input) > 0 and not morse_input.endswith(' '):
+                morse_input += ' '
+                gap = 0
 
-    if keyboard.is_pressed('space'):
-        print
-        gap = 0
-        current = True
-    else:
-        current = False
-        gap += DEBOUNCE_TIME
-    
-    if prev and not current:
-        input = input + '.'
-        print(input)
-    elif prev == current and current == True:
-        input = input + '-'
-        current = False
-        print(input)
+        def image_search(p_text):
+            return search.getImageUrl(p_text)
 
-    if keyboard.is_pressed('enter'):
-        translated = translator.translate_morse(input)
-        print(translated)
-        image_url = imageSearch(translated)
-        print(image_url)
-        OpenImage.openImage(image_url)
-        input = ''
+        while True:
+            add_space()
 
-    if keyboard.is_pressed('backspace'):
-        if (len(input) > 0):
-            input = input[:-1]
-        if (len(input) > 0 and input[-1] == ' '):
-            input = input[:-1]
-        
-        print(input)
+            if keyboard.is_pressed('space'):
+                gap = 0
+                current = True
+            else:
+                current = False
+                gap += DEBOUNCE_TIME
 
-    prev = current
-    time.sleep(DEBOUNCE_TIME) 
+            if prev and not current:
+                morse_input += '.'
+                print(morse_input)
+            elif prev == current and current is True:
+                morse_input += '-'
+                current = False
+                print(morse_input)
+
+            if keyboard.is_pressed('enter'):
+                translated = translator.translate_morse(morse_input)
+                # print(translated)
+                morse_input = ''
+                image_url = image_search(translated)
+                return image_url
+                # print(image_url)
+                # open_image.openImage(image_url)
+
+            if keyboard.is_pressed('backspace'):
+                if len(morse_input) > 0:
+                    morse_input = morse_input[:-1]
+                if len(morse_input) > 0 and morse_input.endswith(' '):
+                    morse_input = morse_input[:-1]
+                print(morse_input)
+
+            prev = current
+            time.sleep(DEBOUNCE_TIME)
