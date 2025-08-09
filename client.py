11eb0dbc6fb0f68f2a756c186b6fd893fdc176da
NikittyCandyCane <Nicole.zhang11409@gmail.com>
2025-08-09 17:24:17 -0400

    created  choosing thing

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 # Import socket module 
 import socket 
-import sys    
-import keyboard        
+import sys          
 
 #keyboard.wait('esc')
 
@@ -33,4 +32,3 @@ while True:
         print('You ended the chat.')
         s.close()
         sys.exit()
-     
